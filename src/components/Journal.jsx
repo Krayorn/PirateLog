e991db1a4cfcbe5f6ec69cbe8dc7105f6024606d
Krayorn <nath.arki36@gmail.com>
2025-01-06 22:50:13 +0100

    add all days + option to have text on right and removed right navigation

--- src/components/Journal.jsx
+++ src/components/Journal.jsx
@@ -5,47 +5,32 @@ export default function Journal({days}) {
     
     return (
         <main className="flex h-screen" >
-            <div className="w-5/6 h-full bg-cyan-500 flex justify-center items-center">
+            <div className="h-full w-full bg-cyan-500 flex justify-center items-center">
                 {openedPage > -1 && 
-                    <Page content={days[openedPage]} />
-                }
-            </div>
-            <div className="w-1/6 h-full bg-yellow-900">
-                {
-                    days.map((day) => {
-                        return <Entry action={() => setPage(day.day-1)} key={day.day} day={day} />
-                    })
+                    <Page prev={() => openedPage >= 1 && setPage(openedPage - 1)} next={() => openedPage < days.length - 1 && setPage(openedPage + 1)} content={days[openedPage]} />
                 }
             </div>
         </main>
     )
 }
 
-function Entry({ day, action }) {
+function Page({ content, prev, next }) {
     return (
-        <div onClick={action} className="h-40 flex justify-center border border-black p-4 bg-yellow-950">
-            <div className="w-full h-full flex justify-center items-center border border-black relative bg-yellow-900">
-                <div className="absolute w-[110%] h-[20px] rotate-[30deg] bg-yellow-950 z-0"></div>
-                <div className="absolute w-[110%] h-[20px] rotate-[-30deg] bg-yellow-950 z-0"></div>
-                <h1  className="text-amber-200 text-3xl z-10">Day {day.day}</h1>
-            </div>
-        </div>
-    )
-}
-
-function Page({ content }) {
-    return (
-        <div className="flex flex-col">
-            <div class="notch bg-[#a67f55] w-full h-[8px]" ></div>
-            <div className="flex" >
-                <div className="bg-[#a67f55] w-[10px]  h-[500px]" ></div>
-                <div className="py-[15px] pr-[15px] pl-[35px] page_left w-[450px] h-[500px]">
-                    {content.text}
+        <div className="flex w-full h-full flex-col items-center justify-center">
+            <div class="notch bg-[#a67f55] w-[calc(80%_+_20px)] h-[8px]" ></div>
+            <div className="flex w-[calc(80%_+_20px)] h-4/5" >
+                <div className="bg-[#a67f55] w-[10px] h-full" ></div>
+                <div className="py-[15px] leading-8 relative pr-[15px] pl-[35px] page_left w-1/2 h-full">
+                    {content.textL}
+                    <span onClick={prev} className="absolute cursor-pointer bottom-[10px] left-[10px]">◀</span>
                 </div>
-                <div className="page_right w-[450px] h-[500px] "></div>
-                <div className="bg-[#a67f55] w-[10px]  h-[500px]" ></div>
+                <div className="relative py-[15px] pl-[15px] pr-[35px] leading-8 page_right w-1/2 h-full">
+                    {content.textR}
+                    <span onClick={next} className="absolute cursor-pointer bottom-[10px] right-[10px]">▶</span>
+                </div>
+                <div className="bg-[#a67f55] w-[10px]  h-full" ></div>
             </div>
-            <div class="notch rotate-[180deg] bg-[#a67f55] w-full h-[8px]" ></div>
+            <div class="notch rotate-[180deg] w-[calc(80%_+_20px)] bg-[#a67f55] h-[8px]" ></div>
         </div>
     )
 }
